0dad3ce203acdb22044d2496939cac892b00890b
Hayate Nakano <nakanodesu@gmail.com>
2020-11-06 14:53:05 +0900

    comment out Zygote test

--- test/runtests.jl
+++ test/runtests.jl
@@ -235,34 +235,34 @@ end
     return x
 end
 
-@testset "Zygote" begin
-    rng = MersenneTwister(1234321)
-    T = ComplexF64
-    a = randn(rng, T, 3, 3)
-    b = randn(rng, T, 3, 3)
-    c = (d = randn(rng, T, 3, 3),)
-    d = randn(rng, T)
-    e = [randn(rng, T, 3, 2, 1, 1, 2, 3) for i = 1:2]
-    f = randn(rng, T, 3, 3)
+# @testset "Zygote" begin
+#     rng = MersenneTwister(1234321)
+#     T = ComplexF64
+#     a = randn(rng, T, 3, 3)
+#     b = randn(rng, T, 3, 3)
+#     c = (d = randn(rng, T, 3, 3),)
+#     d = randn(rng, T)
+#     e = [randn(rng, T, 3, 2, 1, 1, 2, 3) for i = 1:2]
+#     f = randn(rng, T, 3, 3)
 
-    @test foo(a, b, c, d, e, f) ≈ foo_(a, b, c, d, e, f)
+#     @test foo(a, b, c, d, e, f) ≈ foo_(a, b, c, d, e, f)
 
-    for F in [angle ∘ first, angle ∘ sum, real ∘ sum]
-        ad = gradient(
-            (a, b, c, d, e, f) -> F(foo(a, b, c, d, e, f)),
-            (a, b, c, d, e, f)...,
-        )
-        fd = grad(
-            central_fdm(5, 1),
-            (a, b, c, d, e, f) -> F(foo(a, b, c, d, e, f)),
-            (a, b, c, d, e, f)...,
-        )
+#     for F in [angle ∘ first, angle ∘ sum, real ∘ sum]
+#         ad = gradient(
+#             (a, b, c, d, e, f) -> F(foo(a, b, c, d, e, f)),
+#             (a, b, c, d, e, f)...,
+#         )
+#         fd = grad(
+#             central_fdm(5, 1),
+#             (a, b, c, d, e, f) -> F(foo(a, b, c, d, e, f)),
+#             (a, b, c, d, e, f)...,
+#         )
 
-        @test ad[1] ≈ fd[1] atol = cbrt(eps())
-        @test ad[2] ≈ fd[2] atol = cbrt(eps())
-        @test ad[3].d ≈ fd[3].d atol = cbrt(eps())
-        @test ad[4] ≈ fd[4] atol = cbrt(eps())
-        @test ad[5][1] ≈ fd[5][1] atol = cbrt(eps())
-        @test ad[6] ≈ fd[6] atol = cbrt(eps())
-    end
-end
+#         @test ad[1] ≈ fd[1] atol = cbrt(eps())
+#         @test ad[2] ≈ fd[2] atol = cbrt(eps())
+#         @test ad[3].d ≈ fd[3].d atol = cbrt(eps())
+#         @test ad[4] ≈ fd[4] atol = cbrt(eps())
+#         @test ad[5][1] ≈ fd[5][1] atol = cbrt(eps())
+#         @test ad[6] ≈ fd[6] atol = cbrt(eps())
+#     end
+# end
